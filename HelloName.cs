a74b1c044bd5b0f9f97e9c92cb7485f7efabec4b
starlord2015 <noreen.ward@rocketmail.com>
2015-02-05 10:38:17 +0000

    Testing

---
+++ HelloName.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please enter a name!");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+            Console.ReadLine();
+            
+            
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
+
