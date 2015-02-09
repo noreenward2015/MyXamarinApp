2aa09f26d55266997b66d115e16e4ddaacc338cd
starlord2015 <noreen.ward@rocketmail.com>
2015-02-09 22:20:40 +0000

    Business Logic Layer

---
+++ PersonBLL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using $safeprojectname$.DAL;
+
+namespace $safeprojectname$.BLL
+{
+    public class PersonBLL
+    {
+        public DataTable GetPersons()
+        {
+            try
+            {
+                PersonDAL objdal = new PersonDAL();
+                return objdal.Read();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public DataTable GetPersons(Int16 ID)
+        {
+            try
+            {
+                PersonDAL objdal = new PersonDAL();
+                return objdal.Read(ID);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+    }
+}
